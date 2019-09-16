9e6a0d9c91e862f11721af34ff94d9a28a12bcae
Abourass <abourassa@assetval.com>
2019-09-16 15:20:04 -0600

    Complete reformat of JSON

--- app.js
+++ app.js
@@ -41,46 +41,50 @@ app.use(async (ctx, next) => {
 app.use(async ctx => {
   const body = ctx.request.body;
   if (!body) ctx.throw(400, 'body required');
-  const channel = body.channel_id;
-  const slackMsg = {
-    "channel": channel,
-    "blocks": [
-      {
-        "type": "section",
-        "text": {
-          "type": "mrkdwn",
-          "text": "I'm the *coffee cloud*, and I want to know if you would like to get some coffee with everyone?"
-        }
-      },
-      {
-        "type": "actions",
-        "block_id": "coffee_response",
-        "elements": [
-          {
-            "type": "button",
-            "text": {
-              "type": "plain_text",
-              "text": "Yes"
-            },
-            "value": "true",
-            "action_id": "willComeButton",
-            "style": "primary"
-          },
-          {
-            "type": "button",
-            "text": {
-              "type": "plain_text",
-              "text": "No"
-            },
-            "value": "false",
-            "action_id": "wontComeButton",
-            "style": "danger"
+  if (!body.challenge) {
+    const channel = body.channel_id;
+    const slackMsg = {
+      "channel": channel,
+      "blocks": [
+        {
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": "I'm the *coffee cloud*, and I want to know if you would like to get some coffee with everyone?"
           }
-        ]
-      }]
-  };
-  console.log(body);
-  ctx.body = slackMsg;
+        },
+        {
+          "type": "actions",
+          "block_id": "coffee_response",
+          "elements": [
+            {
+              "type": "button",
+              "text": {
+                "type": "plain_text",
+                "text": "Yes"
+              },
+              "value": "true",
+              "action_id": "willComeButton",
+              "style": "primary"
+            },
+            {
+              "type": "button",
+              "text": {
+                "type": "plain_text",
+                "text": "No"
+              },
+              "value": "false",
+              "action_id": "wontComeButton",
+              "style": "danger"
+            }
+          ]
+        }]
+    };
+    console.log(body);
+    ctx.body = slackMsg;
+  } else {
+    ctx.body = body.challenge;
+  }
 });
 
 const port = normalizePort(process.env.PORT || '2777'); // Get port from environment
